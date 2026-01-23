794ce983483f3522b4edf6cf5b9fac46d05d5455
Ricki-h <rick1n.htz@gmail.com>
2026-01-23 06:02:10 -0300

    vai pfv

--- config/database.js
+++ config/database.js
@@ -1,10 +1,8 @@
 const { Sequelize } = require('sequelize');
-const betterSqlite3 = require('better-sqlite3');
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    dialectModule: betterSqlite3,
-    storage: process.env.DB_FILE || 'database.sqlite',
+    storage: process.env.DB_FILE || './data/database.sqlite',
     logging: false,
 });
 
